09d76cad0a938587e7cfd6a57e9f6cce2615eb19
unknown <scripter@BK102PC15.sjca.lan>
2019-04-30 22:16:38 +0200

    les 30042019

---
+++ src/components/DataTabel.vue
@@ -0,0 +1,57 @@
+<template>
+  <table class="w-full">
+    <caption class="bg-blue-500 text-white text-xl p-4">
+      <div class="flex flex-row justify-between items-center">
+        <span class="font-semibold">{{ titel }}</span>
+        <slot name="titel-knop"></slot>
+      </div>
+    </caption>
+    <thead>
+      <tr>
+        <th
+          class="text-left p-2 border-b"
+          v-for="kolom of kolommen"
+          :key="kolom.veld"
+        >
+          {{ kolom.naam }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="border" v-for="item of items" :key="item[itemKey]">
+        <td class="p2" v-for="kolom of kolommen" :key="kolom.veld">
+          {{ item[kolom.veld] }}
+        </td>
+      </tr>
+    </tbody>
+    <tfoot></tfoot>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    titel: {
+      type: String,
+      default: "titel"
+    },
+    items: {
+      type: Array,
+      // idem als
+      // default: function(){
+      // return [] }
+      default: () => []
+    },
+    itemKey: {
+      type: String,
+      default: "_id"
+    },
+    kolommen: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,23 +5,53 @@
       src="@/assets/images/boeken.jpg"
       alt="afbeelding boeken"
     />
-    <table class="w-full">
-      <caption class="bg-blue-500 text-white text-xl p-4">
-        <div class="flex flex-row justify-between items-center">
-          <span class="font-semibold">Boekbeheer</span>
-          <button class="border-2 rounded p-2 hover:bg-blue-300">
-            Nieuw boek
-          </button>
-        </div>
-      </caption>
-    </table>
+    <data-tabel
+      titel="Boekbeheer"
+      :items="boeken"
+      :kolommen="kolommen"
+      itemKey="_id"
+    >
+      <template v-slot:titel-knop>
+        <button class="border-2 rounded p-2 hover:bg-blue-300">
+          Nieuw boek
+        </button>
+      </template>
+    </data-tabel>
   </div>
 </template>
 
 <script>
+import DataTabel from "@/components/DataTabel.vue";
+import axios from "axios";
 // @ is an alias to /src
 export default {
   name: "home",
-  components: {}
+  components: {
+    DataTabel
+  },
+  data() {
+    return {
+      boeken: [],
+      kolommen: [
+        {
+          naam: "Titel",
+          veld: "titel"
+        },
+        {
+          naam: "Auteur",
+          veld: "auteur.naam"
+        },
+        {
+          naam: "Aantal pagina's",
+          veld: "aantalPaginas"
+        }
+      ]
+    };
+  },
+  async created() {
+    //Data ophalen van server
+    const boeken = await axios("http://localhost:7000/boeken");
+    this.boeken = boeken.data;
+  }
 };
 </script>
